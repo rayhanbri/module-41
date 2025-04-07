7cd9ecfbd0ae353d07778d7dd11cd4cc6fcbf18c
rayhanbri <mohammadrayhanx0@gmail.com>
2025-04-07 21:37:19 +0600

    2 added fake data from gpt add fecth it

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import { Suspense } from 'react'
 import './App.css'
+import Bottles from './components/Bottles/Bottles'
+
+
+const bottlesPromise = fetch('./bottle.json')
+.then(res => res.json());
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  // in memory data 
+  // const  waterBottle = [
+  //   {id:1,name:'new nike bollte',price:250,color:'pink'}
+  //   {id:2,name:'new nike bollte',price:250,color:'pink'}
+  //   {id:3,name:'new nike bollte',price:250,color:'pink'}
+  // ]
+
+  // also fake data from gpt 
+ 
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+   
+      <h1>Buy some awsome water Bottle</h1>
+      <Suspense fallback={<h2>Bottles  are loading...</h2>}>
+        <Bottles bottlesPromise={bottlesPromise}></Bottles>
+      </Suspense>
+      
     </>
   )
 }
